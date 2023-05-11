1e1ecff599d729836179f255fc5df9c0f9f8f260
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-11 15:49:17 +0300

    Added skills

--- src/components/ResumeComponents/ExperienceEducation.jsx
+++ src/components/ResumeComponents/ExperienceEducation.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { FaUserGraduate } from "react-icons/fa";
+import { FaBriefcase } from "react-icons/fa";
 
 const ExperienceEducation = () => {
   const experiences = [
-      {
-        id: 1,
+    {
+      id: 1,
       title: "data analyst",
       company: "moringa school",
 
       description: "I was a data analyst at moringa school",
       timeframe: "2022-present",
     },
-      {
-        id: 2,
+    {
+      id: 2,
       title: "data scientist",
       company: "moringa school",
       description: "I was a data scientist at moringa school",
@@ -19,15 +21,15 @@ const ExperienceEducation = () => {
     },
   ];
   const educations = [
-      {
-        id: 1,
+    {
+      id: 1,
       school: "moringa school",
       degree: "data science",
       timeframe: "2020-2021",
       description: "I was a data scientist at moringa school",
     },
-      {
-        id: 2,
+    {
+      id: 2,
       school: "JKUAT",
       degree: "2016-2020",
       timeframe: "2020-2021",
@@ -36,26 +38,63 @@ const ExperienceEducation = () => {
   ];
   return (
     <div>
-      <h1 className="text-4xl font-semibold text-start p-4 text-white">
-        About Me
-      </h1>
-
       <div className="flex  justify-between">
         <div className="flex flex-col w-[50%]">
-          <h1 className="text-2xl p-4 font-semibold text-start">Experience</h1>
+          <h1 className="text-2xl p-4 font-semibold text-start">
+            <FaBriefcase className="inline-block mr-2" />
+            Experience
+          </h1>
           {experiences.map((experience) => (
-            <div className="flex flex-col gap-2">
+            <div
+              className="flex flex-col p-4 gap-2"
+              style={{
+                borderBottom: "1px solid #fff",
+                borderTop: "1px solid #fff",
+              }}
+            >
               <p
                 className={
-                  experience.id === 1 ? " flex justify-center items-center border-green-500 border-2 px-2 w-[40%]" : "bg-red-500 px-2 w-[40%]"
+                  experience.id === 1
+                    ? " flex justify-center items-center border-green-500 border-2 p-2 w-[30%]"
+                    : "border-gray-400 border-2 p-2 w-[30%]"
                 }
               >
                 {experience.timeframe}
               </p>
+              <p>{experience.company}</p>
+              <p>{experience.title}</p>
+              <p>{experience.description}</p>
             </div>
           ))}
         </div>
-        <div className="bg-red-500 w-[50%]">hbj k</div>
+        <div className="flex flex-col w-[50%] border-l-2 border-white">
+          <h1 className="text-2xl p-4 font-semibold text-start">
+            <FaUserGraduate className="inline-block mr-2" />
+            Education
+          </h1>
+          {educations.map((education) => (
+            <div
+              className="flex flex-col p-4 gap-2"
+              style={{
+                borderBottom: "1px solid #fff",
+                borderTop: "1px solid #fff",
+              }}
+            >
+              <p
+                className={
+                  education.id === 1
+                    ? " flex justify-center items-center border-green-500 border-2 p-2 w-[30%]"
+                    : "border-gray-400 border-2 p-2 w-[30%]"
+                }
+              >
+                {education.timeframe}
+              </p>
+              <p>{education.school}</p>
+              <p>{education.degree}</p>
+              <p>{education.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/ResumeComponents/Skills.jsx
+++ src/components/ResumeComponents/Skills.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+  const skills = [
+    {
+      name: "Data Structures",
+      level: 70,
+      difference: 30,
+    },
 
-export default Skills
+    {
+      name: "C++",
+      level: 80,
+      difference: 20,
+    },
+
+    {
+      name: "C++",
+      level: 90,
+      difference: 10,
+    },
+  ];
+  return (
+    <div>
+      <h1 className="text-4xl font-semibold text-start p-4 text-white">
+        Skills
+      </h1>
+      <div className="flex">
+        <div className="flex p-4 flex-col w-[50%]">
+          <h1 className="uppercase">OVERALL</h1>
+          <p className="h-[1px] bg-red-500 w-[70%]  my-2" />
+          {skills.map((skill) => (
+            <div className="flex flex-col my-2">
+              <p>{skill.name}</p>
+              <div className="flex w-[90%] p-2">
+                <p
+                  className={`bg-green-500 h-[8px] w-[${skill.level}%] my-2`}
+                />
+                <p
+                  className={`bg-gray-400 h-[8px] w-[${skill.difference}%] my-2`}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
