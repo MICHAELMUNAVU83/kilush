7a7c30be4660166ce97bcf511267558543a6c317
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-11 16:48:22 +0300

    Added knowledge

--- src/components/ResumeComponents/Skills.jsx
+++ src/components/ResumeComponents/Skills.jsx
@@ -20,12 +20,21 @@ const Skills = () => {
       difference: 30,
     },
   ];
+  const knowldges = [
+    "Data Structures",
+    "C++",
+    "C++",
+    "C++",
+    "C++",
+    "C++",
+    "C++",
+  ];
   return (
     <div>
       <h1 className="text-4xl font-semibold text-start p-4 text-white">
         Skills
       </h1>
-      <div className="flex">
+      <div className="flex ">
         <div className="flex p-4 flex-col w-[50%]">
           <h1 className="uppercase">OVERALL</h1>
           <p className="h-[1px] bg-red-500 w-[70%]  my-2" />
@@ -42,6 +51,25 @@ const Skills = () => {
               </div>
             </div>
           ))}
+        </div>
+        <div className="flex p-4  flex-col w-[50%]">
+          <h1 className="uppercase">KNOWLEDGE</h1>
+          <p className="h-[1px] bg-red-500 w-[70%]  my-2" />
+          <ul>
+            {knowldges.map((knowldge) => (
+              <li className="flex my-2 items-center">
+                <span className="w-4 h-4 mr-2 text-green-500 fill-current">
+                  <svg viewBox="0 0 20 20">
+                    <path
+                      fillRule="evenodd"
+                      d="M0 11l2-2 5 5L18 3l2 2L7 18z"
+                    ></path>
+                  </svg>
+                </span>
+                <span className="text-white text-xl">{knowldge}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
