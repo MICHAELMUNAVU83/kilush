a090ed2bf652c6731a5c5087fd8b7e999958eef5
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-12 13:39:31 +0300

    Added particles

--- src/App.jsx
+++ src/App.jsx
@@ -18,109 +18,7 @@ function App() {
           fpsLimit: 60,
           particles: {
             color: {
-              value: "#fbdb04",
-              animation: {
-                enable: true,
-                speed: 10,
-              },
-            },
-            move: {
-              attract: {
-                enable: true,
-                rotate: {
-                  distance: 100,
-                  x: 2000,
-                  y: 2000,
-                },
-              },
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "destroy",
-              },
-              path: {
-                clamp: false,
-                enable: true,
-                delay: {
-                  value: 0,
-                },
-                generator: polygonPathName,
-                options: {
-                  sides: 6,
-                  turnSteps: 30,
-                  angle: 30,
-                },
-              },
-              random: false,
-              speed: 3,
-              straight: true,
-              trail: {
-                fillColor: "#000",
-                length: 20,
-                enable: true,
-              },
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 0,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: 2,
-            },
-          },
-          background: {
-            color: "#3c5570",
-          },
-          fullScreen: {
-            zIndex: -1,
-          },
-          detectRetina: true,
-          emitters: {
-            direction: "none",
-            rate: {
-              quantity: 1,
-              delay: 0.25,
-            },
-            size: {
-              width: 0,
-              height: 0,
-            },
-            position: {
-              x: 0,
-              y: 0,
-            },
-          },
-        }}
-        init={particlesInit}
-      />
-      <div className="  h-screen flex flex-col justify-center items-center z-10">
-        <div className=" w-[85%] mx-auto text-white">
-          <Router>
-            <SideBar />
-            <Routes>
-              <Route path="/" element={<About />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/resume" element={<Resume />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </Router>
-        </div>
-      </div>
-      <Particles
-        options={{
-          fpsLimit: 60,
-          particles: {
-            color: {
-              value: "#fbdb04",
+              value: "#5FBFF9",
               animation: {
                 enable: true,
                 speed: 10,
@@ -199,6 +97,109 @@ function App() {
             position: {
               x: 100,
               y: 100,
+            },
+          },
+        }}
+        init={particlesInit}
+      />
+      <div className="  h-screen flex flex-col justify-center items-center z-10">
+        <div className=" w-[85%] mx-auto text-white">
+          <Router>
+            <SideBar />
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/resume" element={<Resume />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Router>
+        </div>
+      </div>
+
+      <Particles
+        options={{
+          fpsLimit: 60,
+          particles: {
+            color: {
+              value: "#5FBFF9",
+              animation: {
+                enable: true,
+                speed: 10,
+              },
+            },
+            move: {
+              attract: {
+                enable: true,
+                rotate: {
+                  distance: 100,
+                  x: 2000,
+                  y: 2000,
+                },
+              },
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "destroy",
+              },
+              path: {
+                clamp: false,
+                enable: true,
+                delay: {
+                  value: 0,
+                },
+                generator: polygonPathName,
+                options: {
+                  sides: 6,
+                  turnSteps: 30,
+                  angle: 30,
+                },
+              },
+              random: false,
+              speed: 3,
+              straight: true,
+              trail: {
+                fillColor: "#000",
+                length: 20,
+                enable: true,
+              },
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 0,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: 2,
+            },
+          },
+          background: {
+            color: "#3c5570",
+          },
+          fullScreen: {
+            zIndex: -1,
+          },
+          detectRetina: true,
+          emitters: {
+            direction: "none",
+            rate: {
+              quantity: 1,
+              delay: 0.25,
+            },
+            size: {
+              width: 0,
+              height: 0,
+            },
+            position: {
+              x: 0,
+              y: 0,
             },
           },
         }}
