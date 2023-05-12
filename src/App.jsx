e8a3391ecb99618d8df109ce7d11cb0c8322659f
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-12 13:43:08 +0300

    Added particles

--- src/App.jsx
+++ src/App.jsx
@@ -95,95 +95,6 @@ function App() {
               height: 0,
             },
             position: {
-              x: 100,
-              y: 100,
-            },
-          },
-        }}
-        init={particlesInit}
-      />
-      <Particles
-        options={{
-          fpsLimit: 60,
-          particles: {
-            color: {
-              value: "#5FBFF9",
-              animation: {
-                enable: true,
-                speed: 10,
-              },
-            },
-            move: {
-              attract: {
-                enable: true,
-                rotate: {
-                  distance: 100,
-                  x: 2000,
-                  y: 2000,
-                },
-              },
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "destroy",
-              },
-              path: {
-                clamp: false,
-                enable: true,
-                delay: {
-                  value: 0,
-                },
-                generator: polygonPathName,
-                options: {
-                  sides: 6,
-                  turnSteps: 30,
-                  angle: 30,
-                },
-              },
-              random: false,
-              speed: 3,
-              straight: true,
-              trail: {
-                fillColor: "#000",
-                length: 20,
-                enable: true,
-              },
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 0,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: 2,
-            },
-          },
-          background: {
-            color: "#3c5570",
-          },
-          fullScreen: {
-            zIndex: -1,
-          },
-          detectRetina: true,
-          emitters: {
-            direction: "none",
-            rate: {
-              quantity: 1,
-              delay: 0.25,
-            },
-            size: {
-              width: 0,
-              height: 0,
-            },
-            position: {
               x: 50,
               y: 50,
             },
@@ -191,6 +102,7 @@ function App() {
         }}
         init={particlesInit}
       />
+      
       <div className="  h-screen flex flex-col justify-center items-center z-10">
         <div className=" w-[85%] mx-auto text-white">
           <Router>
@@ -205,95 +117,7 @@ function App() {
         </div>
       </div>
 
-      <Particles
-        options={{
-          fpsLimit: 60,
-          particles: {
-            color: {
-              value: "#5FBFF9",
-              animation: {
-                enable: true,
-                speed: 10,
-              },
-            },
-            move: {
-              attract: {
-                enable: true,
-                rotate: {
-                  distance: 100,
-                  x: 2000,
-                  y: 2000,
-                },
-              },
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "destroy",
-              },
-              path: {
-                clamp: false,
-                enable: true,
-                delay: {
-                  value: 0,
-                },
-                generator: polygonPathName,
-                options: {
-                  sides: 6,
-                  turnSteps: 30,
-                  angle: 30,
-                },
-              },
-              random: false,
-              speed: 3,
-              straight: true,
-              trail: {
-                fillColor: "#000",
-                length: 20,
-                enable: true,
-              },
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 0,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: 2,
-            },
-          },
-          background: {
-            color: "#3c5570",
-          },
-          fullScreen: {
-            zIndex: -1,
-          },
-          detectRetina: true,
-          emitters: {
-            direction: "none",
-            rate: {
-              quantity: 1,
-              delay: 0.25,
-            },
-            size: {
-              width: 0,
-              height: 0,
-            },
-            position: {
-              x: 0,
-              y: 0,
-            },
-          },
-        }}
-        init={particlesInit}
-      />
+     
     </>
   );
 }
